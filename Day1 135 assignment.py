bad29a15bd2c28718c67b0ff3f1f4d8d3fb88d7e
saimallika1409 <saichetkuri289@gmail.com>
2025-02-10 21:16:46 +0530

    Add files via upload

---
+++ Day1 135 assignment.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Declare variables of different data types
+integer_var = 10
+float_var = 3.14
+string_var = "Hello, world!"
+boolean_var = True
+
+# Print each variable and its type
+print("Integer variable:", integer_var, "Type:", type(integer_var))
+print("Float variable:", float_var, "Type:", type(float_var))
+print("String variable:", string_var, "Type:", type(string_var))
+print("Boolean variable:", boolean_var, "Type:", type(boolean_var))
+
+
+# In[ ]:
+
+
+
+
